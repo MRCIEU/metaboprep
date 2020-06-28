a85d638a865ac3aa7785daa77ed0eeb9dd8c6fec
David Hughes <hughes.evoanth@gmail.com>
2020-06-28 16:27:52 +0100

    documentation file for the ng_anno data object

---
+++ data/ng_anno.R
@@ -0,0 +1,18 @@
+#' Nightingale Health metabolite trait annotations
+#' 
+#' An annotation data set for possible Nightingale Health metabolite traits. 
+#' Data in columns include metabolite short name, metabolite long name, metabolite class, 
+#' metabolite subclass, label, label without units, and a binary flag describing the metabolite as
+#' a derived (made up of two or more other data points) metabolite or not. 
+#' 
+#' @docType data
+#'
+#' @usage data(ng_anno)
+#'
+#' @format a data.frame with 233 metabolite traits and 7 annotations
+#'
+#' @keywords datasets
+#'
+#' @source Nightingale Health, <https://nightingalehealth.com/>
+#'
+"ng_anno"
