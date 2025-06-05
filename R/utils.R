4b4f0d06c585f4c702ef6979a10fbdfeac93f985
Nicholas Sunderland <nicholas.p.sunderland@gmail.com>
2025-06-05 18:54:44 +0100

    metaboprep2 hang overs

--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,7 @@
 #' @return A named character vector of available data formats.
 #' @export
 available_data_formats <- function() {
-  r_dir <- system.file(package = "metaboprep2")
+  r_dir <- system.file(package = "metaboprep")
   if (grepl("inst$", r_dir)) {
     r_dir <- sub("/inst","",r_dir) # local package development
   }
@@ -21,7 +21,7 @@ available_data_formats <- function() {
 #' @return A character vector of available report templates
 #' @export
 available_report_templates <- function() {
-  r_dir <- system.file(package = "metaboprep2")
+  r_dir <- system.file(package = "metaboprep")
   templates <- list.dirs(file.path(r_dir, "rmarkdown", "templates"), recursive = FALSE, full.names = FALSE)
   return(templates)
 }
